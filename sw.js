dd167f5f8498ba72be602607e34f6c5c15721b53
gabrielpicca-ship-it <gabrielpicca@gmail.com>
2026-06-30 10:52:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,13 +5,11 @@ const ASSETS = [
   '/manifest.json',
   '/icon.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
